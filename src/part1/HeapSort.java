7458cae64e84d089b8d58d693bbea71962ac3c00
melika barzegaran hosseini <melika.barzegaran.hosseini@gmail.com>
2016-02-05 18:24:20 +0330

    Add heap sort implementation
    this implements an O(nlogn) sorting algorithm in average case named heap sort.

---
+++ src/part1/HeapSort.java
@@ -0,0 +1,60 @@
+package part1;
+
+public class HeapSort extends Sort
+{
+    @Override
+    protected void sort()
+    {
+        heapify();
+
+        int end = values.length - 1;
+        while(end > 0)
+        {
+            swap(0, end);
+            siftDown(0, end - 1);
+            end--;
+        }
+    }
+
+    private void heapify()
+    {
+        int start = (values.length - 2) / 2;
+
+        while(start >= 0)
+        {
+            siftDown(start, values.length - 1);
+            start--;
+        }
+    }
+
+    private void siftDown(int start, int end)
+    {
+        int root = start;
+
+        while((root * 2 + 1) <= end)
+        {
+            int child = root * 2 + 1;
+
+            if(child + 1 <= end && values[child] < values[child + 1])
+            {
+                child = child + 1;
+            }
+            if(values[root] < values[child])
+            {
+                swap(root, child);
+                root = child;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    private void swap(int i, int j)
+    {
+        int temp = values[i];
+        values[i] = values[j];
+        values[j] = temp;
+    }
+}
--- src/part1/SortTest.java
+++ src/part1/SortTest.java
@@ -147,12 +147,13 @@ public class SortTest
     {
         OUTPUT_FOLDER = "output_" + getTime();
 
-        String[] names = {"insertion sort", "quick sort", "merge sort"};
+        String[] names = {"insertion sort", "quick sort", "merge sort", "heap sort"};
 
         ArrayList<ArrayList<Long>> sortTimes = new ArrayList<>();
         sortTimes.add(SortTest.test("insertion-sort", new InsertionSort()));
         sortTimes.add(SortTest.test("quick-sort", new QuickSort()));
         sortTimes.add(SortTest.test("merge-sort", new MergeSort()));
+        sortTimes.add(SortTest.test("heap-sort", new MergeSort()));
 
         SortTest.test(names, sortTimes);
     }
