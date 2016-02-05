95b5e540e441b72169891b6a912a9c2c02edb828
melika barzegaran hosseini <melika.barzegaran.hosseini@gmail.com>
2016-02-05 16:07:47 +0330

    Add sort base implementation
    it contains methods to read from a file and write into its array and write from its array into a file.
    this implementation is restricted to sort just integer numbers.
    each specific sort must extend this base abstract class and implements the abstract method sort.

---
+++ src/part1/Sort.java
@@ -0,0 +1,66 @@
+package part1;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public abstract class Sort
+{
+    protected int[] values;
+
+    protected void read(String path)
+    {
+        File file = new File(path);
+        byte[] bytes = new byte[(int) file.length()];
+        FileInputStream input;
+        try
+        {
+            input = new FileInputStream(file);
+            input.read(bytes);
+            input.close();
+
+            String[] valuesString = new String(bytes).trim().split("\\s+");
+            values = new int[valuesString.length];
+            for(int counter = 0; counter < valuesString.length; counter++)
+            {
+                values[counter] = Integer.parseInt(valuesString[counter]);
+            }
+        }
+        catch (FileNotFoundException e)
+        {
+            System.err.println("error: file '" + path + "' was not found.");
+            System.exit(1);
+        }
+        catch(IOException e)
+        {
+            System.err.println("error: an IO error occurred during reading from the file '" + path + "'.");
+            System.exit(1);
+        }
+    }
+
+    protected void write(String path)
+    {
+        File file = new File(path);
+
+        try
+        {
+            Files.createDirectories(Paths.get(path).getParent());
+            Files.createFile(Paths.get(path));
+
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            for(int value : values)
+            {
+                writer.write(value + " ");
+            }
+            writer.flush();
+            writer.close();
+        }
+        catch (IOException e)
+        {
+            System.err.println("error: file '" + path + "' could not be created/found.");
+            System.exit(1);
+        }
+    }
+
+    protected abstract void sort();
+}
